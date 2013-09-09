d016bb5afb4f39406219887d80045b1ee2a648b0
Matt McCormick <mbmccormick@outlook.com>
2013-09-08 20:54:07 -0700

    Adding some visual loading indicators.

--- HappyHour/MainPage.xaml.cs
+++ HappyHour/MainPage.xaml.cs
@@ -16,40 +16,70 @@ namespace HappyHour
 {
     public partial class MainPage : PhoneApplicationPage
     {
+        #region List Properties
+
         public static ObservableCollection<Item> Venues { get; set; }
+
+        #endregion
+
+        private bool isLoaded = false;
+
         private GeoCoordinateWatcher locationService = null;
 
-        // Constructor
         public MainPage()
         {
             InitializeComponent();
-            Venues = new ObservableCollection<Item>(); 
+            Venues = new ObservableCollection<Item>();
 
-            locationService = new GeoCoordinateWatcher(GeoPositionAccuracy.High); 
+            locationService = new GeoCoordinateWatcher(GeoPositionAccuracy.High);
         }
 
         protected override void OnNavigatedTo(NavigationEventArgs e)
         {
             locationService.Start();
-            LoadData(); 
+
+            if (isLoaded == false)
+                LoadData();
         }
 
         private void LoadData()
         {
+            this.prgLoading.Visibility = System.Windows.Visibility.Visible;
+
             App.HappyHourClient.GetVenues((result) =>
+            {
+                SmartDispatcher.BeginInvoke(() =>
                 {
-                    SmartDispatcher.BeginInvoke(() =>
+                    Venues.Clear();
+
+                    foreach (Item i in result)
                     {
-                        Venues.Clear();
+                        Venues.Add(i);
+                    }
 
-                        foreach (Item i in result)
-                        {
-                            Venues.Add(i); 
-                        }
+                    isLoaded = true;
 
-                        VenueList.ItemsSource = Venues;
-                    });
-                }, locationService.Position.Location.Latitude, locationService.Position.Location.Longitude);
+                    ToggleLoadingText();
+                    ToggleEmptyText();
+
+                    this.prgLoading.Visibility = System.Windows.Visibility.Collapsed;
+                });
+
+            }, locationService.Position.Location.Latitude, locationService.Position.Location.Longitude);
+        }
+
+        private void ToggleLoadingText()
+        {
+            this.txtLoading.Visibility = System.Windows.Visibility.Collapsed;
+            this.lstVenues.Visibility = System.Windows.Visibility.Visible;
+        }
+
+        private void ToggleEmptyText()
+        {
+            if (Venues.Count == 0)
+                this.txtEmpty.Visibility = System.Windows.Visibility.Visible;
+            else
+                this.txtEmpty.Visibility = System.Windows.Visibility.Collapsed;
         }
     }
 }
